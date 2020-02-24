1e75b73a4baab5f348c8671b2671a42ff5251bb8
metin <metin.ozturk>
2020-02-24 09:51:46 +0300

    inputlar alt alta listelenir hale getirildi.

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -161,7 +161,7 @@ export default class CreditCardInput extends Component {
           expiry={expiry}
           cvc={cvc} />
         <ScrollView ref="Form"
-          horizontal
+          vertical
           keyboardShouldPersistTaps="always"
           scrollEnabled={allowScroll}
           showsHorizontalScrollIndicator={false}
